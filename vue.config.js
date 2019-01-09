07555a3535a716af324da169bcff880f76c0bf0b
João Eudes Lima <joaoeudes7@gmail.com>
2019-01-09 17:56:09 -0300

    Update and add config of SCSS

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   productionSourceMap: false,
 
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/styles/_variables.scss";
+          @import "@/styles/_mixins.scss";
+        `
+      }
+    }
+  },
+
   pwa: {
     name: 'VPriSays',
     themeColor: '#4085F5'
